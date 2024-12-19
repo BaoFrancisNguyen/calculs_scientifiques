80ce6e3319c2247d57286002f73c0ed21ec9f36d
api-scraping <bao.francis.nguyen@gmail.com>
2024-12-19 13:53:18 +0100

    adding a dichotomy function to solve equation

---
+++ calculs.py
@@ -0,0 +1,43 @@
+import numpy
+import math
+
+
+#
+
+def f(x):
+    return x**2 - 8 * numpy.log(x)
+x = numpy.array([1, 2, 3, 4, 5])
+y = f(x)
+y = [f(1), f(2), f(3), f(4), f(5)]
+print(y)
+
+def f(x):
+    return x**2 - 8 * math.log(x)
+x = 5
+y = f(x)
+print(y)
+
+left = 1
+right = 2
+précision = 10**-5
+
+while right - left >= précision:
+    middle = (left + right) / 2
+
+    if f(middle) == 0:
+        print(middle)
+        break
+    elif f(middle) * f(left) < 0:
+        right = middle
+    elif f(middle) * f(right) < 0:
+        left = middle
+return middle
+
+if__name__ == '__main__':
+    x = numpy.array([1, 2, 3, 4, 5])
+    y = f(x)
+    middle = solve_equation(f, left)
+
+print('suite')
+print(middle)
+print(f(middle))
